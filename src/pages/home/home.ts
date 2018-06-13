a04a94e5e0468b4e78b7aa2a8477c87fa9d255ae
Josh Sample <joshuawsample@gmail.com>
2018-06-13 17:40:52 -0400

    Temporary digital assets added. Setting default variables in preparation for logic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
+  hydraCI: number;
+  hydraMach: number;
   constructor(public navCtrl: NavController) {
-
+    this.hydraCI = 25000;
+    this.hydraMach = 2;
   }
-
 }
