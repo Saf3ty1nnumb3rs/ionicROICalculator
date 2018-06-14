aa33305b3bc2edd815a408265b933fca76babc2a
Josh Sample <joshuawsample@gmail.com>
2018-06-14 13:43:51 -0400

    Setup for ionic pro initialized

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NavController } from "ionic-angular";
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
-export class HomePage {
+export class HomePage implements OnInit {
   hydraCI: number;
   hydraMach: number;
+  totalCI: number;
+  taxDisc: number;
+  netCapInv: number;
+  cliPerDiem: number;
   constructor(public navCtrl: NavController) {
+    
+  }
+
+  ngOnInit() {
     this.hydraCI = 25000;
     this.hydraMach = 2;
+    this.capitalInvestment();
   }
+
+
+  capitalInvestment() {
+    this.totalCI = this.hydraCI * this.hydraMach;
+  }
+
 }
